cdab079d830da0a816b2441a230a40310e18b733
Ciobanu Nicolae <ciobanu.nicolae.j@gmail.com>
2023-08-24 12:15:47 +0300

    version: 1.0.1

--- huez/packages/index.ts
+++
@@ -1,3 +0,0 @@
-export function huez() {
-  console.log("huez");
-}
---
+++ huez/test/test.ts
@@ -0,0 +1,9 @@
+import { huez } from "../package/index";
+
+const main = () => {
+  const baseColor = "#f2f2f2";
+  const palette = huez(baseColor);
+  console.log(palette);
+};
+
+main();
