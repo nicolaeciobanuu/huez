bbcb6ab69223f8e3f66fd3391bc7e4a02c334615
Ciobanu Nicolae <ciobanu.nicolae.j@gmail.com>
2023-08-31 04:06:39 +0300

    test: add simple test files

---
+++ packages/__tests__/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var src_1 = require("../src");
+var main = function () {
+    var palette = (0, src_1.huez)("#07c", "hex");
+    console.log(palette);
+};
+main();
---
+++ packages/__tests__/index.ts
@@ -0,0 +1,8 @@
+import { huez } from "../src";
+
+const main = () => {
+  const palette = huez("#07c", "hex");
+  console.log(palette);
+};
+
+main();
