1a3be99a338f1f3457311e43ead326ac5519a036
Ciobanu Nicolae <ciobanu.nicolae.j@gmail.com>
2023-09-04 15:28:53 +0300

    feat: add color component

--- www/app/components/color.tsx
+++ www/app/components/color.tsx
@@ -1,4 +1,39 @@
 import styles from "@/styles/color.module.scss";
-export function Color() {
-  return <div>Color</div>;
+export function Color({
+  color,
+  name,
+  format,
+}: {
+  color: any;
+  format: string;
+  name: string;
+}) {
+  var backgroundColor;
+  if (format === "rgb") {
+    const [r, g, b] = color;
+    backgroundColor = "rgb(" + r + ", " + g + ", " + b + ")";
+  } else if (format === "hsl") {
+    var [h, s, l] = color;
+    h = Math.round(h) | 0;
+    s = Math.round(s * 100);
+    l = Math.round(l * 100);
+    backgroundColor = "hsl(" + h + ", " + s + "%, " + l + "%)";
+    ``;
+  } else {
+    backgroundColor = color;
+  }
+
+  return (
+    <div className={styles.color}>
+      <div
+        style={{ backgroundColor: backgroundColor }}
+        onClick={() => navigator.clipboard.writeText(backgroundColor)}
+        className={styles.box}
+      ></div>
+      <div className={styles.flex}>
+        <p className={styles.name}>{name}</p>
+        <p className={styles.value}>{backgroundColor}</p>
+      </div>
+    </div>
+  );
 }
