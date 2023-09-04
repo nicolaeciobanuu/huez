c82277b68ac7e991d71c4bedcd4fb528e76e1f4c
Ciobanu Nicolae <ciobanu.nicolae.j@gmail.com>
2023-09-04 15:28:45 +0300

    feat: add category component

--- www/app/components/category.tsx
+++ www/app/components/category.tsx
@@ -1,4 +1,16 @@
+import { Color } from "./color";
 import styles from "@/styles/category.module.scss";
-export function Category({ children }: { children: React.ReactNode }) {
-  return <div>Category</div>;
+export function Category({
+  children,
+  name,
+}: {
+  children: React.ReactNode;
+  name: string | number;
+}) {
+  return (
+    <div className={styles.category}>
+      <h1 className={styles.name}>{name}</h1>
+      <div className={styles.flex}>{children}</div>
+    </div>
+  );
 }
