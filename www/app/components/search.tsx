1ba6025dd5860700da7349a58a61a573fd67e8e3
Ciobanu Nicolae <ciobanu.nicolae.j@gmail.com>
2023-09-04 15:29:21 +0300

    feat: add search component

--- www/app/components/search.tsx
+++ www/app/components/search.tsx
@@ -1,4 +1,41 @@
 import styles from "@/styles/search.module.scss";
-export function Search() {
-  return <div>Search</div>;
+import { Notation } from "./notation";
+type SearchTypes = {
+  baseColor: string;
+  handleSubmit: Function;
+  changeFormat: Function;
+  changeBaseColor: Function;
+  format: "hsl" | "hex" | "rgb";
+};
+
+export function Search({
+  baseColor,
+  handleSubmit,
+  changeFormat,
+  changeBaseColor,
+  format,
+}: SearchTypes) {
+  return (
+    <div className={styles.container}>
+      <div className={styles.flex}>
+        <div
+          className={styles.box}
+          style={{ backgroundColor: baseColor }}
+        ></div>
+        <Notation format={format} changeFormat={changeFormat} />
+        <input
+          type="text"
+          className={styles.input}
+          value={baseColor}
+          onChange={(event) => changeBaseColor(event.target.value)}
+        />
+        <input
+          type="submit"
+          className={styles.submit}
+          value="Submit"
+          onClick={(event) => handleSubmit(event)}
+        />
+      </div>
+    </div>
+  );
 }
