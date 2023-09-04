44578b1c7112be562717a8904fbae341e440c955
Ciobanu Nicolae <ciobanu.nicolae.j@gmail.com>
2023-09-04 15:29:02 +0300

    feat: add header component

--- www/app/components/header.tsx
+++ www/app/components/header.tsx
@@ -1,4 +1,30 @@
+"use client";
+import { GitHubLogoIcon } from "@radix-ui/react-icons";
 import styles from "@/styles/header.module.scss";
+import Link from "next/link";
 export function Header() {
-  return <div>Header</div>;
+  return (
+    <nav className={styles.navbar}>
+      <ol className={styles.list}>
+        <li className={styles.item}>
+          <Link href="/">Huez</Link>
+        </li>
+        <li className={styles.item}>
+          <Link href="/docs">Docs</Link>
+        </li>
+        <li className={styles.item}>
+          <a
+            title="GitHub"
+            href="https://github.com/ciobanunicolae/huez"
+            target="_blank"
+            rel="noopener noreferrer"
+            className={styles.link}
+            aria-label="GitHub"
+          >
+            <GitHubLogoIcon />
+          </a>
+        </li>
+      </ol>
+    </nav>
+  );
 }
