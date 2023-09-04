be236198a3c993025f843fcba15f950b391aee85
Ciobanu Nicolae <ciobanu.nicolae.j@gmail.com>
2023-09-04 15:29:12 +0300

    feat: add color notation component

--- www/app/components/notation.tsx
+++ www/app/components/notation.tsx
@@ -1,4 +1,23 @@
 import styles from "@/styles/notation.module.scss";
-export function Notation() {
-  return <div>Notation</div>;
+
+export function Notation({
+  changeFormat,
+  format,
+}: {
+  changeFormat: Function;
+  format: "hsl" | "hex" | "rgb";
+}) {
+  return (
+    <select className={styles.select} value={format}>
+      <option value="hex" onClick={() => changeFormat("hex")}>
+        hex
+      </option>
+      <option value="rgb" onClick={() => changeFormat("rgb")}>
+        rgb
+      </option>
+      <option value="hsl" onClick={() => changeFormat("hsl")}>
+        hsl
+      </option>
+    </select>
+  );
 }
