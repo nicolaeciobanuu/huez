2083e0166f8f5410ec146e760ad2085f3e6b2d1b
Ciobanu Nicolae <ciobanu.nicolae.j@gmail.com>
2023-09-19 15:51:33 +0300

    burn: remove header export

--- www/components/index.ts
+++ www/components/index.ts
@@ -1,6 +1,5 @@
 export * from "./category/category";
 export * from "./color/color";
 export * from "./footer/footer";
-export * from "./header/header";
 export * from "./notation/notation";
 export * from "./search/search";
