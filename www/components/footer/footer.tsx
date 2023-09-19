df8242f3dda11e8ecf554b5f7f4634004a305b87
Ciobanu Nicolae <ciobanu.nicolae.j@gmail.com>
2023-09-19 21:28:40 +0300

    feat(footer): add footer component

--- www/components/footer/footer.tsx
+++ www/components/footer/footer.tsx
@@ -1,16 +1,18 @@
+import styles from "./footer.module.scss";
+
 export function Footer() {
   return (
-    <div>
+    <div className={styles.container}>
+      <a
+        target="_blank"
+        rel="nofollow"
+        href="https://github.com/ciobanunicolae/huez"
+      >
+        GitHub
+      </a>
+      <p>Copyright © 2023 Huez</p>
       <div>
-        <a
-          target="_blank"
-          rel="nofollow"
-          href="https://github.com/ciobanunicolae/huez"
-        >
-          GitHub
-        </a>
-      </div>
-      <div>
+        <span>Made by </span>
         <a target="_blank" rel="nofollow" href="https://ciobanunicolae.com">
           ciobanunicolae
         </a>
