bac7148db5c4e78039d711a43bd94a57de5d1340
Ciobanu Nicolae <ciobanu.nicolae.j@gmail.com>
2023-09-19 21:28:19 +0300

    style(font): change font to monserrat

--- www/app/layout.tsx
+++ www/app/layout.tsx
@@ -1,8 +1,8 @@
 import "./globals.css";
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Montserrat } from "next/font/google";
 import { Footer } from "@/components";
-const inter = Inter({ subsets: ["latin"] });
+const font = Montserrat({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Huez",
@@ -16,7 +16,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={font.className}>
         {children}
         <Footer />
       </body>
