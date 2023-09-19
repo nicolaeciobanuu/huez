5aa1d12ca2824b95e805752eb4400712b6a15895
Ciobanu Nicolae <ciobanu.nicolae.j@gmail.com>
2023-09-19 15:50:25 +0300

    chore(layout): add footer and remove header

--- www/app/layout.tsx
+++ www/app/layout.tsx
@@ -1,7 +1,7 @@
-import "./globals.css";
+import "./globals.scss";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import { Header } from "../components/header/header";
+import { Footer } from "@/components";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -17,8 +17,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Header />
         {children}
+        <Footer />
       </body>
     </html>
   );
