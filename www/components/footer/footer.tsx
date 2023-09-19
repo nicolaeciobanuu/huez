4a64e6cc28aeb5e37c39ba098619fef5161f2268
Ciobanu Nicolae <ciobanu.nicolae.j@gmail.com>
2023-09-19 15:51:16 +0300

    feat(layout): add footer component

--- www/components/footer/footer.tsx
+++ www/components/footer/footer.tsx
@@ -1,3 +1,20 @@
 export function Footer() {
-  return <div>Footer</div>;
+  return (
+    <div>
+      <div>
+        <a
+          target="_blank"
+          rel="nofollow"
+          href="https://github.com/ciobanunicolae/huez"
+        >
+          GitHub
+        </a>
+      </div>
+      <div>
+        <a target="_blank" rel="nofollow" href="https://ciobanunicolae.com">
+          ciobanunicolae
+        </a>
+      </div>
+    </div>
+  );
 }
